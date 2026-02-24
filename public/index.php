524b64dc16dd7f74055a53e8fd0abe3f7a84c488
samuel.programmer <ss.samuel.steiner@gmail.com>
2026-02-24 22:10:40 +0100

    move base path initialization out of config bootstrap

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../config/bootstrap.php';
+require_once __DIR__ . '/../bootstrap/app.php';
 require_once BASE_PATH . '/vendor/autoload.php';
 
 use App\Foundation\Routing\Router;
